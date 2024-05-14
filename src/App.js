4e1b5c2e514b4c16f1b631e25ac586917fb27f14
Tehreem Fazili <tehreemfazili5@gmail.com>
2024-05-14 17:13:34 +0530

    MRA-app-drawer: Styled app drawer

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import Header from './Components/Header/Header.jsx';
+import DrawerLeft from './Components/Drawer/Drawer.jsx';
 
 function App() {
   return (
@@ -6,6 +7,7 @@ function App() {
       <header className="App-header">
         <Header/>
       </header>
+      <DrawerLeft />
     </div>
   );
 }
--- src/Components/Drawer/Drawer.jsx
+++ src/Components/Drawer/Drawer.jsx
@@ -1,48 +1,48 @@
 import * as React from 'react';
 import './Drawer.scss'
-import Box from '@mui/material/Box';
 import Drawer from '@mui/material/Drawer';
-import CssBaseline from '@mui/material/CssBaseline';
-import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
 import List from '@mui/material/List';
-import Typography from '@mui/material/Typography';
 import Divider from '@mui/material/Divider';
 import ListItem from '@mui/material/ListItem';
 import ListItemButton from '@mui/material/ListItemButton';
 import ListItemIcon from '@mui/material/ListItemIcon';
 import ListItemText from '@mui/material/ListItemText';
-import InboxIcon from '@mui/icons-material/MoveToInbox';
-import MailIcon from '@mui/icons-material/Mail';
 import { IconButton } from '@mui/material';
-import { ArrowRight } from '@mui/icons-material';
+import { ArrowRight, Close, Download, Error, PersonOutlineOutlined} from '@mui/icons-material';
 
-const drawerWidth = 240;
 
 export default function DrawerLeft() {
   return (
-    <Drawer sx={{ width: drawerWidth, margin: '24px 50px' }} variant="permanent" anchor="left" className='drawer'>
-      <Toolbar className='drawer-toolbar'>
-        <IconButton/> | <div className='logout-btn'>Logout</div>
+    <Drawer sx={{ '& .MuiPaper-root': { overflow: 'visible', backgroundColor: 'rgb(216, 191, 216, 0.25)' } }} variant="permanent" anchor="left" className='drawer'>
+      <Toolbar className='drawer-toolbar' sx={{ width: '100%'}}>
+        <IconButton> <PersonOutlineOutlined/> </IconButton> | <div className='logout-btn'>Logout</div>
+        <IconButton sx={{ marginLeft: 'auto', backgroundColor: 'rgb(216, 191, 216, 0.25)', padding: '8px', position: 'absolute', right: '13px', borderRadius: '0 100% 100% 0'}}> <Close /> </IconButton>
       </Toolbar>
-      <List>
-      {['Dashboard', 'Retailers', 'Always On', 'Event', 'Resources', 'Messages', 'Bookings'].map((text, index) => (
-         <ListItem key={text} disablePadding sx={{ display: 'flex', flexDirection: 'column', alignItems: 'start' }}>
-          <div sx={{ display: 'flex', alignItems: 'center'}} className='drawer-item-wrapper' >
-            <ListItemButton>
-              <ListItemText primary={text} />
-            </ListItemButton>
-            <ListItemIcon>
-            <ArrowRight/>
+      <List sx={{ margin: '0 32px' }}>
+        {['Dashboard', 'Retailers', 'Always On', 'Event', 'Resources', 'Messages', 'Bookings'].map((text, index) => (
+          <ListItem key={text} disablePadding sx={{ display: 'flex', alignItems: 'center' }}>
+            <div  className='drawer-item-wrapper' >
+              <ListItemButton sx={{ padding: '8px 0'}}>
+                <ListItemText primary={text} />
+              </ListItemButton>
+              <Divider style={{width:'100%'}} />  
+            </div>
+            <ListItemIcon sx={{ justifyContent: 'end'}}>
+              <ArrowRight/>
             </ListItemIcon>
-          </div>
-         <Divider style={{width:'100%'}} />
-         </ListItem>
-       ))}
-     </List>
-    </Drawer>
+          </ListItem>
+        ))}
+        
+          <ListItem sx={{  padding: '8px 0'}}><ListItemIcon sx={{ alignItems: 'center'  }}>
+            <Download sx={{ paddingRight: '16px'}}/> <p> Download Guide</p>
+          </ListItemIcon> </ListItem>
+          <ListItem sx={{  padding: '8px 0' }}><ListItemIcon sx={{ alignItems: 'center'}}>
+            <Error sx={{ paddingRight: '16px'}}/> <p> Help and support</p>
+          </ListItemIcon> </ListItem>
+      </List>
+     </Drawer>
   )
-    // <Box sx={{ display: 'flex' }}>
     //   <CssBaseline />
     //   <AppBar
     //     position="fixed"
