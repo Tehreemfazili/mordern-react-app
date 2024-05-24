21d73953454babb9cb851c01d1a8926c6a017913
Tehreem Fazili <tehreemfazili5@gmail.com>
2024-05-24 12:50:45 +0530

    MRA-menu: Handling submenus in app drawer

--- src/Components/Drawer/Drawer.jsx
+++ src/Components/Drawer/Drawer.jsx
@@ -15,23 +15,21 @@ import { DrawerItems } from './DrawerItems.tsx';
 
 
 export default function DrawerLeft() {
-  const [item, setItem] = useState([])
   const [open, setOpen] = useState(true);
+  const [openItems, setOpenItems] = useState({});
 
-  function handleItemClick(link) {
-  //   // console.log('item clicked', item)
-  //   // <Link to= {link} />
-    console.log(link)
-  //   // setOpen(false)
-  //   // navigate(`/${link}`)
+  const handleItemClick = (link) => {
+    console.log(link);
+  };
 
-
-  }
-  
-  function handleSubItem() {
-    setOpen(!open);
-    console.log(open)
-  }
+  const handleSubItemClick = (name) => {
+    setOpenItems((prevOpenItems) => ({
+      ...prevOpenItems,
+      [name]: !prevOpenItems[name],
+    }));
+    // console.log("openItems",openItems)
+    setOpen(!open)
+  };
 
 
   const drawerItems = DrawerItems;
@@ -45,44 +43,73 @@ export default function DrawerLeft() {
       </Toolbar>
       
       <List sx={{ margin: '0 32px' }}>
-      
-        {/* { drawerItems.map((text, index) => (
-          <ListItem key={text.name} disablePadding sx={{ display: 'flex', alignItems: 'center' }} onClick={handleItemClick(text.name, text.link)} >
-              <div  className='drawer-item-wrapper' >
-                <ListItemButton sx={{ padding: '8px 0'}} onClick={console.log('clicked')}>
-                  <ListItemText primary={text.name} onClick={ console.log('clicked text')} />
-                </ListItemButton>
-                <Divider style={{width:'100%'}} />  
-              </div>
-                { text.subItem ? <ListItemIcon sx={{ justifyContent: 'end'}}>
-                      <ArrowRight/> </ListItemIcon> : '' }
-            </ListItem>
-          ))}  */}
-         
-
+{/* 
         { drawerItems.map((text, index) => (
+         <>
           <ListItem key={text.name} disablePadding sx={{ display: 'flex', alignItems: 'center' }}  >
               <div  className='drawer-item-wrapper' >
-                <ListItemButton sx={{ padding: '8px 0'}} onClick={ text.subItem != null ?  handleSubItem : handleItemClick(text.link) }>
-                  <ListItemText primary={text.name}/>
-                </ListItemButton>
+                <ListItemButton sx={{ padding: '8px 0'}} onClick={ text.subItem != null ?  handleSubItemClick : handleItemClick(text.link) }>
+                  <Link to={text.link} className='drawer-items'>{text.name}</Link>
+                   <ListItemText primary={text.name} /> 
+
+                </ListItemButton> 
                 
-                <Divider style={{width:'100%'}} />  
+               <Divider style={{width:'100%'}} />  
               </div>
                 { text.subItem ?   <ListItemIcon sx={{ justifyContent: 'end'}}> { open ? 
                  <ArrowDropDown/> : <ArrowRight/>  } </ListItemIcon>  : '' } 
-                 <Collapse in={open} timeout="auto" unmountOnExit>
-                  <List component="div" disablePadding>
-                  { text.subItem && text.subItem.map((text) => {
-                      // {console.log('subitem - ',text.name)}
-                      <ListItemButton>
-                        <ListItemText primary={text.name} />
-                      </ListItemButton>
-                  })}
-                  </List>
-                </Collapse>
+                 
           </ListItem>
-          ))} 
+          <Collapse in={open} timeout="auto" unmountOnExit>
+          <List component="div" disablePadding>
+          { text.subItem && text.subItem.map((text) => {
+              
+              <ListItemButton>
+                <ListItemText primary={text.name} />
+              </ListItemButton>
+          })}
+          </List>
+        </Collapse>
+         </>
+          ))}   */}
+
+         {drawerItems.map((item) => (
+          <div key={item.name} className="drawer-item-wrapper">
+            <ListItem disablePadding sx={{ display: 'flex', alignItems: 'center' }}>
+              <ListItemButton
+                sx={{ padding: '8px 0' }}
+                onClick={item.subItem ? () => handleSubItemClick(item.name) : () => handleItemClick(item.link)}
+              >
+                {item.subItem ? (
+                  <>
+                    <ListItemText primary={item.name} />
+                    <ListItemIcon sx={{ justifyContent: 'end' }}>
+                      {openItems[item.name] ? <ArrowDropDown /> : <ArrowRight />}
+                    </ListItemIcon>
+                  </>
+                ) : (
+                  <Link to={item.link} className="drawer-items">
+                    {item.name}
+                  </Link>
+                )}
+              </ListItemButton>
+            </ListItem>
+            {item.subItem && (
+              <Collapse in={openItems[item.name]} timeout="auto" unmountOnExit>
+                <List component="div" disablePadding>
+                  {item.subItem.map((subItem) => (
+                    <ListItemButton key={subItem.name} sx={{ pl: 4 }} onClick={handleItemClick(subItem.link)}>
+                      <Link to={subItem.link} className="drawer-items">
+                        {subItem.name}
+                      </Link>
+                    </ListItemButton>
+                  ))}
+                </List>
+              </Collapse>
+            )}
+            <Divider style={{ width: '100%' }} />
+          </div>
+        ))} 
           <ListItem sx={{  padding: '8px 0'}}><ListItemIcon sx={{ alignItems: 'center'  }}>
             <Download sx={{ paddingRight: '16px'}}/> <p> Download Guide</p>
           </ListItemIcon> </ListItem>
